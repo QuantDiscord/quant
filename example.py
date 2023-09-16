771edfbebe4f92925ed0b3feac784a0cf3b2ed2c
MagM1go <68418026+MagM1go@users.noreply.github.com>
2023-09-16 18:33:39 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,6 @@
-import asyncio
-
 from impl.core.client import Client
 from data.intents import Intents
 from impl.events.bot.ready_event import ReadyEvent
-from impl.core.commands import Command
 
 
 client = Client(
@@ -17,12 +14,12 @@ client = Client(
 async def test_command_callback(bot: Client) -> None:
     await bot.rest.create_message(channel_id=..., content="aboba")
 
+
 async def on_ready(event: ReadyEvent):
     print('started')
 
 
-client.commands["test"] = test_command_callback
+client.add_command(command_name="test", coro=test_command_callback)
 client.add_listener(ReadyEvent, on_ready)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(client.run())
+client.run()
