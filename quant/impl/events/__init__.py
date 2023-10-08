c6cf277b36410bec315ea665f82233591c916ddf
MagM1go <magmigo2@inbox.ru>
2023-10-08 16:42:19 +0800

    remove useless import

--- quant/impl/events/__init__.py
+++ quant/impl/events/__init__.py
@@ -2,5 +2,5 @@ from .event import BaseEvent
 from .types import EventTypes
 from .guild import *
 from .guild.message_events import *
-from .guild.member_events import *
+# from .guild.member_events import *
 from .bot import *
