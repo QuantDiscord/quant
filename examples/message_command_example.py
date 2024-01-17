414f0d099fd4a26f6d5f5a25bd5328f3bebbad6c
MagM1go <68418026+MagM1go@users.noreply.github.com>
2024-01-17 16:23:50 +0800

    change example

--- examples/message_command_example.py
+++ examples/message_command_example.py
@@ -1,34 +1,29 @@
-from quant.impl.core.context import MessageCommandContext
-from quant.impl.core.commands import MessageCommand
-from quant.entities.guild.messages.mentions.allowed_mentions import AllowedMentions
-from quant.entities.guild.messages.mentions.allowed_mentions_types import AllowedMentionsTypes
-from quant.impl.core.client import Client
-from quant.entities.intents import Intents
+import quant
 
 
-client = Client(prefix="!", intents=Intents.ALL_UNPRIVILEGED, token="...")
+client = quant.Client(prefix="!", intents=Intents.ALL_UNPRIVILEGED, token="...")
 
 
-async def say_command_callback(context: MessageCommandContext, *message: str) -> None:
-    allowed_mentions = AllowedMentions(parse=[AllowedMentionsTypes.ALL.value])
+async def say_command_callback(context: quant.InteractionContext, *message: str) -> None:
+    allowed_mentions = quant.AllowedMentions(parse=[quant.AllowedMentionsTypes.ALL.value])
     await context.send_message(content=" ".join(message), allowed_mentions=allowed_mentions)
 
 
-say_command = MessageCommand(name="say", description="Say something")
-say_command.callback = say_command_callback
+say_command = quant.SlashCommand(name="say", description="Say something")
+say_command.set_callback(say_command_callback)
 
 client.add_message_command(say_command)
 client.run()
 
 
 # Or you can make like this:
-class SayCommand(MessageCommand):
+class SayCommand(quant.SlashCommand):
     def __init__(self) -> None:
         super().__init__("say", "say something")
 
-    async def callback(self, context: MessageCommandContext, *message: str) -> None:
+    async def callback(self, context: quant.InteractionContext, *message: str) -> None:
         await context.send_message(content=" ".join(message))
 
 
-client.add_message_command(SayCommand())
+client.add_slash_command(SayCommand())
 client.run()
